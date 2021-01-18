40d460fb27acc688812c9555d0fcddfbd475d9f0
Matt <matt.smith3108@gmail.com>
2021-01-18 17:28:06 +0000

    Added in animated hamburger menu as well as an animated mobile menu with overlay

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,22 @@
-console.log('hello world');
+const body = document.querySelector('body');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const fadeElements = document.querySelectorAll('.has-fade');
+
+header.addEventListener('click', function () {
+	if (header.classList.contains('open')) {
+		body.classList.remove('noscroll');
+		header.classList.remove('open');
+		fadeElements.forEach(function (element) {
+			element.classList.remove('fade-in');
+			element.classList.add('fade-out');
+		});
+	} else {
+		body.classList.add('noscroll');
+		header.classList.add('open');
+		fadeElements.forEach(function (element) {
+			element.classList.remove('fade-out');
+			element.classList.add('fade-in');
+		});
+	}
+});
