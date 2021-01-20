3d388e49441521a6d61f8fa1843e4cc12238658f
Matt <matt.smith3108@gmail.com>
2021-01-20 16:26:43 +0000

    Adding in a responsive 4 column layout with flexbox for the feature section

--- app/js/script.js
+++ app/js/script.js
@@ -3,7 +3,7 @@ const btnHamburger = document.querySelector('#btnHamburger');
 const header = document.querySelector('.header');
 const fadeElements = document.querySelectorAll('.has-fade');
 
-header.addEventListener('click', function () {
+btnHamburger.addEventListener('click', function () {
 	if (header.classList.contains('open')) {
 		body.classList.remove('noscroll');
 		header.classList.remove('open');
